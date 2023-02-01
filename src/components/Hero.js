d5e1e5bd1e95d330abe7e565e8730dcd2d788791
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-02-01 23:43:01 +0600

    components added

---
+++ src/components/Hero.js
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+import { useHoverEffect } from "../hooks/useHoverEffect";
+
+const data = {
+  img1: "https://images.pexels.com/photos/4061512/pexels-photo-4061512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  img2: "https://images.pexels.com/photos/2460488/pexels-photo-2460488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
+
+const Hero = () => {
+  const heroImageRef = useRef(null);
+
+  useHoverEffect(heroImageRef, data.img1, data.img2);
+
+  return (
+    <div className='hero container mx-auto mt-20'>
+      <div className='hero-image' ref={heroImageRef}></div>
+      <div className='shutter'>
+        <h2>ReactJS</h2>
+      </div>
+      <div className='shutter'>
+        <h2>Developer</h2>
+      </div>
+      <div className='circle-left'></div>
+      <div className='circle-right'></div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
+import Hero from "./Hero";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
