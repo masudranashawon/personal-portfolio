7d63eaaf6c99df1618eb05804dddf161aa19b2cc
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-02-08 00:46:47 +0600

    Custom cursor added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Socials from "./components/Socials";
+import CustomCursor from "./components/CustomCursor";
+import { useRef } from "react";
+import { useCustomCursor } from "./hooks/useCustomCursor";
 
 const App = () => {
+  const innerCursorRef = useRef(null);
+  const outerCursorRef = useRef(null);
+
   useSmoothScroll();
+  useCustomCursor(innerCursorRef, outerCursorRef);
 
   return (
     <div className='app'>
       <div className='noise'></div>
+      <CustomCursor
+        innerCursorRef={innerCursorRef}
+        outerCursorRef={outerCursorRef}
+      />
       <Navbar />
       <Socials />
       <Routes>
