6f6df8b46c6ad452662da9d0168f2c6562bf37d2
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-02-08 00:02:48 +0600

    Social added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import { useSmoothScroll } from "./hooks/useSmoothScroll";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Socials from "./components/Socials";
 
 const App = () => {
+  useSmoothScroll();
+
   return (
     <div className='app'>
       <div className='noise'></div>
       <Navbar />
+      <Socials />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,5 +1,39 @@
+const data = [
+  {
+    id: 1,
+    title: "Facebook",
+    url: "https://www.facebook.com/masudranashawon786",
+  },
+  { id: 2, title: "Tweetter", url: "https://twitter.com/MasudRanaShawo1" },
+  { id: 3, title: "GitHub", url: "https://github.com/masudranashawon" },
+  {
+    id: 4,
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/masudranashawon1/",
+  },
+  {
+    id: 5,
+    title: "Instagram",
+    url: "https://instagram.com/masudranashawon786",
+  },
+];
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className='socials bg-black/30 p-5 flex gap-5 uppercase text-sm fixed left-10 top-[75%] backdrop-blur-xl origin-left -rotate-90 z-[200] text-white/30'>
+      {data.map((social) => (
+        <a
+          key={social.id}
+          href={social.url}
+          target='_blank'
+          rel='noreferrer'
+          className='hover:text-cyan-400 duration-500'
+        >
+          {social.title}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
